b15c34f65948ca09b66343636d0742e8ebc1d037
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-02 07:59:07 +0300

    Update layout and main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,19 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Maps from './components/Maps';
+import Feature from './components/Feature';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 export default function Home() {
-	return <div className="">Home page</div>;
+	return (
+		<div>
+			<Navbar />
+			<Hero />
+			<Maps />
+			<Feature />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
